a01f6203715625c3ea4af38c7e07df506feb2e15
Kamis-sinha <kamilleleitevieira@gmail.com>
2025-02-25 15:01:01 -0300

    Add files via upload

--- d6.js
+++ d6.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
     $(".container").mouseenter(function(){
         $(".card").stop().animate({
-            top:"-90px"
-        },"slow");
+            top: "-90px"
+        }, "slow");
     }).mouseleave(function(){
-        $(."card").stop().animate({
-            top:0
-        },"slow");
-    })
-    })
+        $(".card").stop().animate({
+            top: "0px"
+        }, "slow");
+    });
+});
+
+$(".container").mouseenter(function(){
+    $(".card, .heart").stop().animate({
+        top: "-90px"
+    }, "slow");
+}).mouseleave(function(){
+    $(".card, .heart").stop().animate({
+        top: "0"
+    }, "slow");
+});
